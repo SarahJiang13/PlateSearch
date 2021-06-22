e0c5070625dce5d67d624cc6d63c270a9de74ba8
Your Name <you@example.com>
2021-06-22 13:48:21 +1000

    Upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask, render_template
+from flask import request, redirect
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/button', methods=['POST'])
+def button():
+     num = 5
+     return redirect(5*num) 
+     
+@app.route('/number', methods=['POST'])
+def number():
+    num=request.form('Number')
+    return str(5*5)
+    
+	   
+if __name__ == '__main__':
+    app.run()
+
+
