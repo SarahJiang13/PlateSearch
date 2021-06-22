e14be22bcee5051c887c9ab70ed2eee98c702f55
RichieVann4567 <86278254+RichieVann4567@users.noreply.github.com>
2021-06-23 09:27:17 +1000

    Add files via upload

---
+++ main(the better one).py
@@ -0,0 +1,46 @@
+import os.path
+
+from flask import Flask, Response
+
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+
+def root_dir(): 
+    return os.path.abspath(os.path.dirname(__file__))
+
+
+def get_file(filename):  
+    try:
+        src = os.path.join(root_dir(), filename)
+       
+        return open(src).read()
+    except IOError as exc:
+        return str(exc)
+
+
+@app.route('/', methods=['GET'])
+def metrics():  
+    content = get_file('website.html')
+    return Response(content, mimetype="text/html")
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def get_resource(path):  
+    mimetypes = {
+        ".css": "text/css",
+        ".html": "text/html",
+        ".js": "application/javascript",
+    }
+    complete_path = os.path.join(root_dir(), path)
+    ext = os.path.splitext(path)[1]
+    mimetype = mimetypes.get(ext, "text/html")
+    content = get_file(complete_path)
+    return Response(content, mimetype=mimetype)
+
+
+if __name__ == '__main__':  
+    app.run()
+
